d09c59274e4ecee7175133759436b7b523015e63
YuhuaYang <yhyecho@gmail.com>
2017-03-03 11:53:04 +0800

    commit

--- myblog/index.js
+++ myblog/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+    res.send('Hello Express2')
 })
 
 app.listen(3000, () => {
