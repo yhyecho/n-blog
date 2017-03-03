d1d87b0f227ff8c7cf3f22099629c95f054f439a
YuhuaYang <yhyecho@gmail.com>
2017-03-03 13:45:58 +0800

    add ejs view engine

--- myblog/index2.js
+++ myblog/index2.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/users')
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.use('/', indexRouter)
 app.use('/users', userRouter)
 
--- myblog/routes/users.js
+++ myblog/routes/users.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/:name', (req, resp) => {
-    resp.send(`Hello ${req.params.name}`)
+    // resp.send(`Hello ${req.params.name}`)
+    // 渲染ejs模版
+    resp.render('users', {
+        name: req.params.name
+    })
 })
 
 module.exports = router
